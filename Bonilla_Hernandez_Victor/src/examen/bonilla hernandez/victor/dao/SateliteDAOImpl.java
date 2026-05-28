0acd130096b4d3f326d0351d2e354279eda6ffdf
victorbnllahdez <a29312@svalero.com>
2026-05-28 10:38:13 +0200

    Add files via upload

--- Bonilla_Hernandez_Victor/src/examen/bonilla hernandez/victor/dao/SateliteDAOImpl.java
+++ Bonilla_Hernandez_Victor/src/examen/bonilla hernandez/victor/dao/SateliteDAOImpl.java
@@ -84,7 +84,300 @@ public class SateliteDAOImpl
 
     private static final String SQL_FIND_BY_GENERO =
             "SELECT * " +
-                    "FROM peliculas " +
-                    "WHERE genero = ? " +
-                    "ORDER BY titulo";
+                    "FROM satelites " +
+                    "WHERE agencia = ? ";
+
+
+
+    public void check() {
+        try {
+            motorSQL.connect();
+            if (motorSQL.conn != null &&
+                    !motorSQL.conn.isClosed()) {
+                System.out.println("CONEXION OK");
+            }
+        } catch (Exception e) {
+            printError(e);
+        } finally {
+            motorSQL.close();
+        }
+    }
+
+    public SateliteDAOImpl(
+            MotorSQL motorSQL) {
+        super(motorSQL);
+    }
+
+
+    @Override
+    public void add(Satelite satelite) {
+        try{
+            motorSQL.connect();
+            motorSQL.prepare(SQL_INSERT);
+            motorSQL.getPs().setString(1, satelite.getNombre());
+            motorSQL.getPs().setString(2, satelite.getOrbita());
+            motorSQL.getPs().setString(3, satelite.getPeso());
+            motorSQL.getPs().setString(4, satelite.getCoste());
+            motorSQL.getPs().setString(5, satelite.getActivo());
+
+            int rows = motorSQL.executeUpdate();
+            System.out.println(
+                    "INSERTADOS: " +
+                            rows);
+
+
+        }catch (Exception e){
+
+            printError(e);
+
+        }finally {
+
+            motorSQL.close();
+        }
+    }
+
+    @Override
+    public void update(
+            int id,
+            Satelite satelite) {
+        try{
+            motorSQL.connect();
+            motorSQL.prepare(SQL_UPDATE);
+            motorSQL.getPs().setString(
+                    1,
+                    satelite.getNombre());
+            motorSQL.getPs().setString(
+                    2,
+                    satelite.getOrbita());
+            motorSQL.getPs().setString(
+                    3,
+                    satelite.getPeso());
+            motorSQL.getPs().setString(
+                    4,
+                    satelite.getCoste());
+            motorSQL.getPs().setString(
+                    5,
+                    satelite.getActivo());
+            motorSQL.getPs().setString(
+                    6,
+                    id);
+            int rows =
+                    motorSQL.executeUpdate();
+            System.out.println(
+                    "ACTUALIZADOS: " +
+                            rows);
+        }catch (Exception e){
+            printError(e);
+        }finally {
+            motorSQL.close();
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+
+        try{
+
+            motorSQL.connect();
+
+            motorSQL.prepare(SQL_DELETE);
+
+            motorSQL.getPs().setInt(
+                    1,
+                    id);
+
+            int rows =
+                    motorSQL.executeUpdate();
+
+            System.out.println(
+                    "BORRADOS: " +
+                            rows);
+
+        }catch (Exception e){
+
+            printError(e);
+
+        }finally {
+
+            motorSQL.close();
+        }
+    }
+
+    @Override
+    public Satelite find(int id) {
+
+        Satelite satelite = null;
+
+        try{
+            motorSQL.connect();
+            motorSQL.prepare(SQL_FIND);
+            motorSQL.getPs().setInt(
+                    1,
+                    id);
+            ResultSet rs =
+                    motorSQL.executeQuery();
+            if(rs.next()){
+
+                satelite =
+                        mapSatelite(rs);
+            }
+
+        }catch (Exception e){
+
+            printError(e);
+
+        }finally {
+
+            motorSQL.close();
+        }
+
+        return satelite;
+    }
+
+    @Override
+    public ArrayList<Satelite> findAll() {
+
+        ArrayList<Satelite> satelites =
+                new ArrayList<>();
+
+        try{
+
+            motorSQL.connect();
+
+            motorSQL.prepare(SQL_FIND_ALL);
+
+            ResultSet rs =
+                    motorSQL.executeQuery();
+
+            while(rs.next()){
+
+                satelite.add(
+                        mapSatelite(rs));
+            }
+
+        }catch (Exception e){
+
+            printError(e);
+
+        }finally {
+
+            motorSQL.close();
+        }
+
+        return satelites;
+    }
+    @Override
+    public ArrayList<Satelite> findByAgencia(Agencia agencia) {
+
+        ArrayList<Satelite> satelites =
+                new ArrayList<>();
+
+        try{
+
+            motorSQL.connect();
+
+            motorSQL.prepare(
+                    SQL_FIND_BY_AGENCIA);
+
+            motorSQL.getPs().setString(
+                    1,
+                    agencia);
+
+            ResultSet rs =
+                    motorSQL.executeQuery();
+
+            while(rs.next()){
+
+                satelites.add(
+                        mapSatelites(rs));
+            }
+
+        }catch (Exception e){
+
+            printError(e);
+
+        }finally {
+
+            motorSQL.close();
+        }
+
+        return satelites;
+    }
+
+    @Override
+    public Satelite findDetalleSateliteBySatelite(int idSatelite) {
+        Satelite satelite = new Satelite();
+        try{
+            motorSQL.connect();
+            motorSQL.prepare(
+                    SQL_DETALLE_SATELITE);
+            motorSQL.getPs().setInt(
+                    1,
+                    idSatelite);
+            ResultSet rs =
+                    motorSQL.executeQuery();
+            if(rs.next()){
+                pelicula.setId(rs.getInt(1));
+                pelicula.setNombre(rs.getString(2));
+                pelicula.setOrbita(rs.getString(3));
+                pelicula.setPeso(rs.getString(4));
+                pelicula.setCoste(rs.getString(5));
+                pelicula.setActivo(rs.getString(6));
+
+                pelicula.getDetalleSatelite().
+                        setId(rs.getInt(7));
+                pelicula.getDetalleSatelite().
+                        setText(rs.getString(8));
+
+                pelicula.getDetalleSatelite().
+                        setPresupuesto(rs.getLong(9));
+                pelicula.getDetalleSatelite().
+                        setRecaudacion(rs.getLong(10));
+                pelicula.getDetalleSatelite().
+                        setIdioma(rs.getString(11));
+            }
+        }catch (Exception e){
+            printError(e);
+        }finally {
+            motorSQL.close();
+        }
+        return satelite;
+    }
+    private Satelite mapSatelite(
+            ResultSet rs)
+            throws Exception {
+
+        Satelite satelite =
+                new Satelite();
+
+        satelite.setId(
+                rs.getInt("id"));
+
+        satelite.setTitulo(
+                rs.getString("titulo"));
+
+        satelite.setDirector(
+                rs.getString("director"));
+
+        satelite.setGenero(
+                rs.getString("genero"));
+
+        satelite.setAnyo(
+                rs.getInt("anyo"));
+
+        satelite.setDuracion(
+                rs.getInt("duracion"));
+
+        return pelicula;
+    }
+    public static void main(String[] args){
+        PeliculaDAOImpl peliculaDAO =
+                new PeliculaDAOImpl(MotorFactory.
+                        create(MotorFactory.ORACLE));
+    }
+
+
+
+
+
         }
